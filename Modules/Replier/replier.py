24792d0360318e82c38edf31847c9ec9c2ead188
Abhinav Suresh Ennazhiyil <abhinav@Abhinavs-MacBook-Pro.local>
2022-03-09 19:50:33 +0400

    Modified replier.py

--- Modules/Replier/replier.py
+++ Modules/Replier/replier.py
@@ -6,16 +6,22 @@ class Replier:
 
     def __init__(self, message):
         self.message = message
+        self.probability_of_triggered_reply = 10
+        self.probability_of_normal_reply = 20
+        self.probability_of_question_reply = 4
 
     def run(self):
         if self.message.content[-1] == '?':
-            return "Perhaps"
-        p = 10 if any(t in self.message.content.lower() for t in self.TRIGGER_WORDS) else 20
-        r = random.randint(1, p)
-        print(r)
-        if r == 1:
-            return f"Your face is {self.message}"
-        if r == 2:
-            return "Dint knew"
-        if r == 3:
-            return "Really?"
+            r = random.randint(1, self.probability_of_question_reply)
+            if r == 1:
+                return "Perhaps"
+        else:
+            p = self.probability_of_triggered_reply if any(t in self.message.content.lower() for t in self.TRIGGER_WORDS) else self.probability_of_normal_reply
+            r = random.randint(1, p)
+            print(r)
+            if r == 1:
+                return f"Your face is {self.message}"
+            if r == 2:
+                return "Dint knew"
+            if r == 3:
+                return "Really?"
