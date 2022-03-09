18228716ad7cc8b8ad1b79a9259e835263bf7cac
Abhinav Suresh Ennazhiyil <abhinav@Abhinavs-MacBook-Pro.local>
2022-03-09 19:43:48 +0400

    Modified replier.py

--- Modules/Replier/replier.py
+++ Modules/Replier/replier.py
@@ -8,10 +8,14 @@ class Replier:
         self.message = message
 
     def run(self):
-        p = 5 if any(t in self.message.content for t in self.TRIGGER_WORDS) else 10
+        if self.message.content[-1] == '?':
+            return "Perhaps"
+        p = 10 if any(t in self.message.content.lower() for t in self.TRIGGER_WORDS) else 20
         r = random.randint(1, p)
         print(r)
         if r == 1:
             return f"Your face is {self.message}"
         if r == 2:
-            return "Dint knew."
+            return "Dint knew"
+        if r == 3:
+            return "Really?"
