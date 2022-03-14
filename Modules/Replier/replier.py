f156dceeb4c24b9a1283c142eef38610c754e9f7
Abhinav Suresh Ennazhiyil <abhinav@Abhinavs-MacBook-Pro.local>
2022-03-14 23:19:57 +0400

    Added dice.py in Utils

--- Modules/Replier/replier.py
+++ Modules/Replier/replier.py
@@ -1,5 +1,5 @@
 import random
-
+from Utils.dice import Dice
 
 class Replier:
     TRIGGER_WORDS = []
@@ -9,23 +9,20 @@ class Replier:
         self.probability_of_normal_reply = 40
         self.probability_of_question_reply = 6
 
+        self.dice_question = Dice(self.probability_of_question_reply)
+        self.dice_question.add_value("Perhaps")
+
     def parse(self, message):
         if message.content:
             if message.content[-1] == '?':
-                r = random.randint(1, self.probability_of_question_reply)
-                if r == 1:
-                    return "Perhaps"
+                self.dice_question.roll()
             else:
-                p = self.probability_of_triggered_reply if any(t in message.content.lower() for t in self.TRIGGER_WORDS) else self.probability_of_normal_reply
-                r = random.randint(1, p)
-                print(r)
-                if r == 1:
-                    return f"Your face is {message.content}"
-                if r == 2:
-                    return "Dint knew"
-                if r == 3:
-                    return "Really?"
-                if r == 4:
-                    return "sus"
-                if r == 5:
-                    return "nou"
+                dice_reply = Dice(self.probability_of_normal_reply)
+                dice_reply.add_value(f"Your face is {message.content}")
+                dice_reply.add_value("Dint knew")
+                dice_reply.add_value("Really?")
+                dice_reply.add_value("sus")
+                dice_reply.add_value("nou")
+                if any(t in message.content.lower() for t in self.TRIGGER_WORDS):
+                    dice_reply.sides = self.probability_of_triggered_reply
+                print(dice_reply.roll())
