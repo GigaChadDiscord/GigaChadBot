857cc9aa931d83f9dba7e817476856de2a9530cf
Abhinav Suresh Ennazhiyil <abhinav@Abhinavs-MacBook-Pro.local>
2022-03-22 18:58:57 +0530

    try catch

--- Modules/Reddit/reddit.py
+++ Modules/Reddit/reddit.py
@@ -21,7 +21,7 @@ class Reddit:
         # send our request for an OAuth token
         res = requests.post('https://www.reddit.com/api/v1/access_token',
                             auth=auth, data=data, headers=self.headers)
-
+        print(res)
         # convert response to JSON and pull access_token value
         TOKEN = res.json()['access_token']
 
